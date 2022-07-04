9e570f7d9c57570d0e6543060bb9c9f11ed55480
484phaks <bonganivusi@rocketmail.com>
2022-07-04 09:45:52 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - This is a description
+ *
+ *   *
+ *
+ *    * Return: a number 0 if successful
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
