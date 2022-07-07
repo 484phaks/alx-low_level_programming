94a1c092e2f8924ee740f51f6905b3c4a2990a96
484phaks <bonganivusi@rocketmail.com>
2022-07-06 21:20:38 -0700

    dd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-* print_alphabet -> prints the lowercase alphabets
+* print_alphabet - entry point
+* Return: 0 Success
 */
 
-
-void print_alphabet(voidi);
+void print_alphabet(void)
 
 {
 
-	int j;
+	char j;
 
 
 
