429282f23fd5329c87c1b9edeab3dee44d04b450
484phaks <bonganivusi@rocketmail.com>
2022-07-06 18:24:23 -0700

    ddd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
-nclude "main.h"
+#include "main.h"
 
 /**
-
- * print_alphabet - Prints the alphabet in lowercase
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+* print_alphabet - Prints the alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
 
 
 void print_alphabet(void)
