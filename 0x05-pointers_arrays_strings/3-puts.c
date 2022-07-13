7dcfb747e9717f52c6f0d38de53ddf9275a1ecdb
484phaks <bonganivusi@rocketmail.com>
2022-07-13 10:34:28 -0700

    dd

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#innclude "main.h"
+
+/**
+* _puts - check the code.
+* @str: String to be printed out
+* Return: Always 0.
+*/
+
+void _puts(char *str)
+
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+
+}
