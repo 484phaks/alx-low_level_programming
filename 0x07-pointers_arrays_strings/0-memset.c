20a1931b6f30eeab7dc56eea574a8b17aecad46e
484phaks <bonganivusi@rocketmail.com>
2022-07-19 19:16:09 -0700

    ff

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _memset 
-* @s: pointer to char params
-* @b: data to change
-* @n: index
-* Return: *s
+* _memset - prints buffer in hexa
+* @s: The array
+* @b: What the array should be filled
+* @n: How many bytes of the array should be filled
+* Return: Nothing.
 */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -15,11 +15,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	for (i = 0; i < n; i++)
 
-	{
-
 		s[i] = b;
-
-	}
 
 	return (s);
 }
