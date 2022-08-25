08f0b26e63cdc87a3a48af1895fbf8fc2e570924
484phaks <bonganivusi@rocketmail.com>
2022-08-25 15:34:16 -0700

    dd

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,20 +11,25 @@
  * Return: the address of the new node, or NULL if it
  * failed.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
 {
 	unsigned int i;
 	listint_t *new;
 	listint_t *h;
+
 	h = *head;
 
 	if (idx != 0)
+
 	{
 		for (i = 0; i < idx - 1 && h != NULL; i++)
 		{
 			h = h->next;
 		}
 	}
+
 	if (h == NULL && idx != 0)
 		return (NULL);
 
@@ -35,14 +40,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 
 	if (idx == 0)
+
 	{
 		new->next = *head;
 		*head = new;
 	}
+
 	else
+
 	{
 		new->next = h->next;
 		h->next = new;
 	}
+
 	return (new);
 }
