99bcd25741273fa421bb7575322ef6d1777e2371
484phaks <bonganivusi@rocketmail.com>
2022-08-25 15:44:07 -0700

    tt" git commit -m "dd" clear git commit -m tt git commit -m dd clear
    git commit -m 'tt

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	h = *head;
 
 	if (idx != 0)
-
 	{
 		for (i = 0; i < idx - 1 && h != NULL; i++)
 		{
@@ -32,26 +31,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (h == NULL && idx != 0)
 		return (NULL);
-
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
 
 	if (idx == 0)
-
 	{
 		new->next = *head;
 		*head = new;
 	}
-
 	else
-
 	{
 		new->next = h->next;
 		h->next = new;
 	}
-
 	return (new);
 }
