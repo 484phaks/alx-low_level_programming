ebc48e22306a70c514276f9f2a4fad76f24679b2
484phaks <bonganivusi@rocketmail.com>
2022-08-25 15:53:34 -0700

    dd

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * free_listp2 - frees a linked list
  * @head: head of a list.
@@ -18,7 +17,6 @@ void free_listp2(listp_t **head)
 	{
 		curr = *head;
 		while ((temp = curr) != NULL)
-
 		{
 			curr = curr->next;
 			free(temp);
@@ -33,17 +31,15 @@ void free_listp2(listp_t **head)
  *
  * Return: size of the list that was freed.
  */
-
 size_t free_listint_safe(listint_t **h)
 
 {
 	size_t nnodes = 0;
 	listp_t *hptr, *new, *add;
 	listint_t *curr;
+
 	hptr = NULL;
-
 	while (*h != NULL)
-
 	{
 		new = malloc(sizeof(listp_t));
 
@@ -56,7 +52,6 @@ size_t free_listint_safe(listint_t **h)
 		add = hptr;
 
 		while (add->next != NULL)
-
 		{
 			add = add->next;
 			if (*h == add->p)
@@ -67,15 +62,14 @@ size_t free_listint_safe(listint_t **h)
 				return (nnodes);
 			}
 		}
+
 		curr = *h;
 		*h = (*h)->next;
 		free(curr);
 		nnodes++;
-
 	}
+
 	*h = NULL;
 	free_listp2(&hptr);
-
 	return (nnodes);
-
 }
